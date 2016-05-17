73167eac6845c984cc9536e7ff5714a6213c6b53
Swedish-li <1993770815@qq.com>
2016-05-17 20:26:27 +0800

    删除多余配置，代码清理

--- src/main/java/com/lrs/controller/AreaController.java
+++ src/main/java/com/lrs/controller/AreaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.jarvis.cache.annotation.Cache;
 import com.lrs.pojo.Area;
 import com.lrs.service.AreaService;
 
