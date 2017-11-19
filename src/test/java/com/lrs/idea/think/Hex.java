0b2be1260f7f6326af5d516c6f155fd6824ac889
Swedish-li <one.piece.li.2016@gmail.com>
2017-11-19 15:28:31 +0800

    add example

---
+++ src/test/java/com/lrs/idea/think/Hex.java
@@ -0,0 +1,46 @@
+package com.lrs.idea.think;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.*;
+import java.net.URL;
+
+public class Hex {
+
+    @Test
+    public void testFormat() throws Exception {
+        byte[] data = read();
+        StringBuilder rs = new StringBuilder();
+        int count = 0;
+        for (byte b : data){
+            if (count % 16 == 0){
+                // 行的头部信息
+                rs.append(String.format("%05x:",count));
+            }
+            rs.append(String.format("%02x ",b));
+            count++;
+            // 判断是否到达行尾
+            if (count%16 ==0){
+                rs.append('\n');
+            }
+
+        }
+
+        System.out.print(rs);
+
+    }
+
+    private byte[] read() throws IOException{
+        URL url = Hex.class.getResource("Hex.class");
+
+        BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
+        try {
+            byte[] data = new byte[inputStream.available()];
+            inputStream.read(data);
+            return data;
+        }finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+    }
+}
