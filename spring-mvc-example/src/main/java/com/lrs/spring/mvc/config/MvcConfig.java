f1caa0888528e6d4f617eae9c4c80572003488ae
Swedish-li <one.piece.li.2016@gmail.com>
2018-03-09 15:17:56 +0800

    modify package scan config

--- spring-mvc-example/src/main/java/com/lrs/spring/mvc/config/MvcConfig.java
+++ spring-mvc-example/src/main/java/com/lrs/spring/mvc/config/MvcConfig.java
@@ -5,17 +5,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan( value = "com.lrs.spring.mvc")
+@ComponentScan(value = "com.lrs.spring.mvc",
+        includeFilters = {@ComponentScan.Filter({Controller.class,
+                RestController.class,
+                ControllerAdvice.class})},
+        useDefaultFilters = false)
 public class MvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public ExampleInterceptor exampleInterceptor(){
+    public ExampleInterceptor exampleInterceptor() {
         return new ExampleInterceptor();
     }
 
